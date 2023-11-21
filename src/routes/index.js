65b6bbfaef9a9840a2b043634dd7ad205279ae2e
JailsonWin2 <jailson.ferens@aluno.unc.br>
2023-11-21 14:33:03 -0300

    Inserindo rota de apolices no index de routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,14 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
 import autores from "./autoresRoutes.js";
+import apolices from "../models/Apolice.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
-    res.status(200).send({titulo: "Curso de node"});
+    res.status(200).send({ titulo: "Curso de node" });
   });
 
-  app.use(
-    express.json(),
-    livros,
-    autores
-  );
+  app.use(express.json(), livros, autores, apolices);
 };
 
 export default routes;
