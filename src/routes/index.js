69970fcc9b910ea061d5cbb91efb8a23ab8fe05c
JailsonWin2 <jailson.ferens@aluno.unc.br>
2023-11-29 14:27:31 -0300

    Adicionando as configurações do CORS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import apolices from "./apolicesRoutes.js";
 import comissoes from "./comissoesRoutes.js";
 
 const routes = (app) => {
+  app.use(cors()); // Adicione esta linha para habilitar o CORS
+
   app.route("/").get((req, res) => {
     res.status(200).send({ titulo: "Curso de node" });
   });
